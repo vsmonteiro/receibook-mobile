fec88f3fdcbfb3950997e14c55ea420920474f9a
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2021-02-14 23:45:45 -0300

    Add icon for hiding password

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,20 +6,31 @@ import { MaterialIcons } from '@expo/vector-icons';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [eyeIcon, setEyeIcon] = useState("visibility-off");
+  const [isPassword, setIsPassword] = useState(true);
+
+  const changePwdType = () => {
+    setEyeIcon(isPassword ? "visibility" : "visibility-off");
+    setIsPassword((prevState: boolean) => !prevState);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.formContainer}>
         <TextInput placeholder='E-mail' style={styles.input} value={email} onChangeText={(t) => setEmail(t)} />
-        <TextInput
-          placeholder='Senha'
-          style={styles.input}
-          textContentType='password'
-          secureTextEntry
-          value={password}
-          onChangeText={(t) => setPassword(t)}
-        />
-        <MaterialIcons name='remove-red-eye' />
+
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder='Senha'
+            style={styles.passwordInput}
+            textContentType='password'
+            secureTextEntry={isPassword}
+            value={password}
+            onChangeText={(t) => setPassword(t)}
+          />
+
+          <MaterialIcons style={styles.icon} onPress={changePwdType} name={eyeIcon} size={28} />
+        </View>
 
         <RectButton style={styles.loginButton}>
           <Text style={styles.loginText}> Login </Text>
@@ -58,6 +69,20 @@ const styles = StyleSheet.create({
     fontSize: 18
   },
 
+  passwordInput: {
+    backgroundColor: '#fff',
+    borderWidth: 1.4,
+    borderColor: '#D43737',
+    borderRadius: 20,
+    height: 56,
+    paddingVertical: 18,
+    paddingHorizontal: 24,
+    marginBottom: 16,
+    textAlignVertical: 'top',
+    fontSize: 18,
+    flex: 1
+  },
+
   loginButton: {
     backgroundColor: '#D43737',
     borderRadius: 20,
@@ -68,7 +93,7 @@ const styles = StyleSheet.create({
   },
 
   loginText: {
-    fontFamily: 'Nunito_800ExtraBold',
+    fontFamily: 'Nunito_700Bold',
     fontSize: 16,
     color: '#FFF',
   },
@@ -81,5 +106,15 @@ const styles = StyleSheet.create({
   registerText: {
     color: 'blue',
     fontSize: 18
+  },
+
+  passwordContainer: {
+    flexDirection: 'row',
+  },
+
+  icon: {
+    position: 'absolute',
+    right: 20,
+    top: 15
   }
 })
